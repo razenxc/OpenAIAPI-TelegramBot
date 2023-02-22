87e60f15bfa6c32aa8529ea08986b8b54fce195e
Alexsey <84779107+razenxc@users.noreply.github.com>
2023-02-22 13:21:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require("openai");
 
@@ -13,7 +13,13 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.command('start', async (ctx) => {
     try {
-        await ctx.reply('This bot for using OpenAI API features in Telegram bot!\nUse /help for detailed info!')
+        await ctx.reply('This bot for using OpenAI API features in Telegram bot!\nUse /help for detailed info!', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Help', 'helpBtn'), Markup.button.callback('Ping', 'pingBtn')],
+                [Markup.button.callback('Generate', 'genImage')]
+            ]
+        ))
+        console.log(ctx.message)
     } catch (error) {
         console.error(error)   
     }
@@ -35,10 +41,49 @@ bot.command('help', async (ctx) => {
     }
 } )
 
-bot.command('ask', async (ctx) => {
+function createButtonReplyCommand(btnName, command) {
+    bot.action(btnName, async (ctx) => {
+        try {
+            await ctx.answerCbQuery()
+            await ctx.reply(command)
+        } catch (error) {
+            console.error(error)
+        }
+    })
+}
+
+
+bot.command('generate', async (ctx) => {
     try {
         // code
+        await ctx.reply('Choose what to generate', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Image',  'genImageBtn'), Markup.button.callback('Text',  'genTextBtn')]
+            ]
+        ))
     }  catch(error) {
+        console.error(error)
+    }
+})
+
+
+
+bot.action('genImageBtn', async (ctx) => {
+    try {
+        await ctx.answerCbQuery()
+        await ctx.reply('Enter what do you need to generate')
+        bot.on('text', async (ctx) => {
+            console.log(ctx.message.text)
+            const response = await openai.createImage({
+                prompt: ctx.message.text,
+                n: 1,
+                size: "1024x1024",
+              });
+              image_url = response.data.data[0].url;
+              await ctx.replyWithPhoto(response.data.data[0].url, { caption: 'Generated image: ' + ctx.message.text })
+        })
+        
+    } catch (error) {
         console.error(error)
     }
 })
